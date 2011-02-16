12ebaa8c306d586543acc71943ba464d94fbdef1
Davide Principi <davide.principi@nethesis.it>
2011-02-16 18:11:54 +0100

    Added a base implementation of a panel composite: StandardCompositePanel.

--- application/libraries/StandardPanel.php
+++ application/libraries/StandardPanel.php
@@ -3,9 +3,7 @@
 abstract class StandardPanel implements PanelInterface, PolicyEnforcementPointInterface {
 
     private $identifier;
-
     private $parameters;
-
     /**
      * @var PolicyDecisionPointInterface;
      */
@@ -26,14 +24,14 @@ abstract class StandardPanel implements PanelInterface, PolicyEnforcementPointIn
         return $this->identifier;
     }
 
-    public function render()
-    {
-     
-    }
-
     public function validate()
     {
-
+        return true;
+    }
+    
+    public function render()
+    {
+        return "";
     }
 
     public function setPolicyDecisionPoint(PolicyDecisionPointInterface $pdp)
@@ -41,8 +39,62 @@ abstract class StandardPanel implements PanelInterface, PolicyEnforcementPointIn
         $this->policyDecisionPoint = $pdp;
     }
 
+    public function getPolicyDecisionPoint()
+    {
+        return $this->policyDecisionPoint;
+    }
+
+    /**
+     * Renders a PHP view by means of Code Igniter Framework.
+     * @param string $viewName Name of the view .php file under `views/` directory.
+     * @param array $parameters View parameters
+     * @return string View output
+     */
     protected function renderView($viewName, $parameters = array())
     {
         return CI_Controller::get_instance()->load->view($viewName, $parameters, true);
     }
+
+}
+
+abstract class StandardCompositePanel extends StandardPanel implements PanelCompositeInterface {
+
+    private $children = array();
+
+    public function getChildren()
+    {
+        return array_values($this->children);
+    }
+
+    public function addChild(PanelInterface $panel)
+    {
+        $this->children[$panel->getIdentifier()] = $panel;
+
+        if ($this instanceof PolicyEnforcementPointInterface
+                && $panel instanceof PolicyEnforcementPointInterface
+                && ! is_null($this->getPolicyDecisionPoint()))
+        {
+            $panel->setPolicyDecisionPoint($this->getPolicyDecisionPoint());
+        }
+    }
+
+    /**
+     * Default implementation of a composite panel forwards the rendering
+     * process to child panels.
+     *
+     * @return string 
+     */
+    public function render()
+    {
+        $output = '';
+        foreach ($this->getChildren() as $panel)
+        {
+            if ($panel instanceof PanelInterface)
+            {
+                $output .= $panel->render();
+            }
+        }
+        return $output;
+    }
+
 }
