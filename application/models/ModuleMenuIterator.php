549a76fc87fa1753fc48728d3a3f055ec979b937
Davide Principi <davide.principi@nethesis.it>
2011-02-23 15:09:06 +0100

    * Moved ModuleMenuIterator class into its own file.

---
+++ application/models/ModuleMenuIterator.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ *
+ */
+final class ModuleMenuIterator implements RecursiveIterator {
+
+    private $elements;
+    /**
+     * @var ModuleSetInterface
+     */
+    private $moduleSet;
+    private $pointer;
+    private $key;
+
+
+    public function __construct(ModuleSetInterface $moduleSet, $pointer, &$elements = array())
+    {
+        $this->elements = $elements;
+        $this->pointer = $pointer;
+        $this->moduleSet = $moduleSet;
+    }
+
+    public function current()
+    {
+        return $this->moduleSet->findModule($this->currentIdentifier());
+    }
+
+    private function currentIdentifier()
+    {
+        return $this->elements[$this->pointer][$this->key];
+    }
+
+    public function getChildren()
+    {
+        return new self($this->moduleSet, $this->currentIdentifier(), $this->elements);
+    }
+
+    public function hasChildren()
+    {
+        return isset($this->elements[$this->currentIdentifier()]) && is_array($this->elements[$this->currentIdentifier()]) && ! empty($this->elements[$this->currentIdentifier()]);
+    }
+
+    public function key()
+    {
+        return $this->key;
+    }
+
+    public function next()
+    {
+        $this->key ++;
+    }
+
+    public function rewind()
+    {
+        $this->key = 0;
+    }
+
+    public function valid()
+    {
+        return isset($this->elements[$this->pointer][$this->key]);
+    }
+
+}
