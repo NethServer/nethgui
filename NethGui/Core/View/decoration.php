51aa287f503bfd17ab24f6153ddd1e6c800f16f4
Davide Principi <davide.principi@nethesis.it>
2011-03-04 09:48:12 +0100

    Added a decoration DIV allWrapper and a touch of CSS.

--- NethGui/Core/View/decoration.php
+++ NethGui/Core/View/decoration.php
@@ -3,21 +3,25 @@
     <head>
         <title>NethGui</title>
         <link rel="stylesheet" type="text/css" href="<?php echo $css_main; ?>" />
-<?php foreach($js as $scriptPath): ?>
-        <script type="text/javascript" src="<?php echo $scriptPath ?>" ></script>
-<?php endforeach ?>
+        <?php foreach ($js as $scriptPath): ?>
+            <script type="text/javascript" src="<?php echo $scriptPath ?>" ></script>
+        <?php endforeach ?>
     </head>
     <body>
-        <div id="header">NethGui</div>
-        <div class="colmask leftmenu">
-            <div class="colleft">
-                <div class="col1">
-                    <div id="breadcrumbMenu"><?php echo $breadcrumb_menu ?></div>
-                    <div id="moduleContent"><?php echo $module_content ?></div> 
+        <div id="allWrapper">
+            <div id="header">NethGui</div>
+            <div class="colmask leftmenu">
+                <div class="colleft">
+                    <div class="col1">
+                        <div id="breadcrumbMenu"><?php echo $breadcrumb_menu ?></div>
+                        <div id="moduleContent"><?php echo $module_content ?></div>
+                    </div>
+                    <div class="col2"><div id="moduleMenu"><?php echo $module_menu ?></div></div>
                 </div>
-                <div class="col2"><div id="moduleMenu"><?php echo $module_menu ?></div></div>
+            </div>
+            <div id="footer">
+                Powered by <a href="<?php echo site_url('../doc') ?>">NethGuiFramework</a> &ndash; Copyright 2011 &copy; Nethesis S.r.l
             </div>
         </div>
-        <div id="footer">Powered by <a href="<?php echo site_url('../doc') ?>">NethGuiFramework</a> &ndash; Copyright 2011 &copy; Nethesis S.r.l</div>
     </body>
 </html>
