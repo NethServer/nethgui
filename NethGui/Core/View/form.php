9c77181f54723137d0848ee486d8cbc4ab0103c9
Davide Principi <davide.principi@nethesis.it>
2011-03-10 16:37:37 +0100

    * Sample Ajax request/response JSON format.

--- NethGui/Core/View/form.php
+++ NethGui/Core/View/form.php
@@ -3,4 +3,3 @@ echo form_open_multipart(uri_string());
 echo $framework->renderView('NethGui_Core_View_container', $self);
 echo '<div style="text-align: right"><input id="' . $id['save'] . '" name="' . $name['save'] . '" type="submit" value="Save" /></div>';
 echo form_close();
-?>
--- NethGui/Core/View/json.php
+++ NethGui/Core/View/json.php
@@ -1,3 +1,2 @@
 <?php
 echo json_encode($data);
-?>
