55cb2c747fc80a2c2259f5b0b2d2ca7558c7dd99
Davide Principi <davide.principi@nethesis.it>
2011-03-31 14:55:06 +0200

    Removed debugging code.

--- NethGui/View/RemoteAccess/RemoteManagement.php
+++ NethGui/View/RemoteAccess/RemoteManagement.php
@@ -1,6 +1,5 @@
 <h2>Gestione Remota</h2>
-<p>
-    <?php echo __FILE__ ?>
+<p>    
     <small>E' possibile consentire l'accesso a computer su reti remote al
     server-manager, inserendo le reti abilitate qui. Utilizzare una
     subnet mask di 255.255.255.255 per limitare l'accesso ad un host specifico.
