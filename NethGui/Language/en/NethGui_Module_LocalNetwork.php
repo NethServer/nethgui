ff78e520e5e0dc2ec98a3a0931a58f8cc0be99d4
Davide Principi <davide.principi@nethesis.it>
2011-05-05 18:41:01 +0200

    Added string translations in Table Delete and Read actions.
    * Added language catalog for LocalNetwork Module.
    * Added translation of module title in Menu & BreadCrumbs

---
+++ NethGui/Language/en/NethGui_Module_LocalNetwork.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @package Language
+ * @subpackage Core
+ */
+
+$L['LocalNetwork_Title'] = 'Local networks';
+$L['network_label'] = 'Network address';
+$L['Mask_label'] = 'Network mask';
+$L['Router_label'] = 'Router address';
+$L['SystemLocalNetwork_label'] = 'System network';
+$L['NethGui_Module_LocalNetwork_caption'] = 'Local networks';
