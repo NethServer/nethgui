218de9b98674e283cb028bfd294317fdf482bd88
Davide Principi <davide.principi@nethesis.it>
2011-05-20 12:46:42 +0200

    Refs #137. Implemented fallback language catalog.
    Refactor of NotificationArea and DialogBox view.

---
+++ NethGui/Template/NotificationAreaDialogBox.php
@@ -0,0 +1,33 @@
+<?php
+
+$dialogClass = 'dialog';
+
+switch ($view['type']) {
+    case NethGui_Core_DialogBox::NOTIFY_SUCCESS:
+        $dialogClass .= ' embedded success';
+        break;
+    case NethGui_Core_DialogBox::NOTIFY_WARNING:
+        $dialogClass .= ' embedded warning';
+        break;
+    case NethGui_Core_DialogBox::NOTIFY_ERROR:
+        $dialogClass .= ' modal error';
+        break;
+}
+
+?><div class="<?php 
+    echo $dialogClass 
+?>" id="Dialog_<?php 
+    echo $view['dialogId']
+?>"><span class="message"><?php
+    echo htmlspecialchars(T($view['message']));
+?></span><?php
+
+if(count($view['actions']) > 0):
+    ?><ul class="actions"><?php
+    foreach ($view['actions'] as $action) :
+        ?><li><?php echo $action ?></li><?php
+    endforeach;
+    ?></ul><?php
+endif;
+
+?></div>
