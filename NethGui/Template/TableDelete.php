bdc1d6f2d3dca0537028e18e532d502d91e72a89
Davide Principi <davide.principi@nethesis.it>
2011-06-10 17:26:24 +0200

    Wrapped table-delete buttons into an @UL.actions@ tag.

--- NethGui/Template/TableDelete.php
+++ NethGui/Template/TableDelete.php
@@ -6,8 +6,9 @@ $message = T('Confirm deletion of "%s"?',
 echo $view
         ->append($message) // Add the dialog text (see $message)
         ->hidden($view['__key']) // Put the key value into an hidden control
-        ->button('Submit', NethGui_Renderer_Xhtml::BUTTON_SUBMIT) // Add SUBMIT button
-        ->button('Cancel', NethGui_Renderer_Xhtml::BUTTON_CANCEL) // Add CANCEL button
-;
-
-?>
+    ;
+?><ul class="actions"><li><?php 
+    echo $view->button('Submit', NethGui_Renderer_Xhtml::BUTTON_SUBMIT) // Add SUBMIT button 
+        ?></li><li><?php
+    echo $view->button('Cancel', NethGui_Renderer_Xhtml::BUTTON_CANCEL) // Add CANCEL button
+        ?></li></ul>
