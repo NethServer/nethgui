f8dc37642a75b1090ecebb99310854c21d8bf242
Davide Principi <davide.principi@nethesis.it>
2011-06-22 15:02:40 +0200

    Refs #95. Added Unit test for ArrayAccessSerializer.
    Fixed the creation of a new record, if the key doesn't exist.

--- NethGui/Serializer/ArrayAccessSerializer.php
+++ NethGui/Serializer/ArrayAccessSerializer.php
@@ -11,38 +11,53 @@
  */
 class NethGui_Serializer_ArrayAccessSerializer implements NethGui_Serializer_SerializerInterface
 {
+
     private $prop;
     private $key;
-    
     /**
      *
      * @var ArrayAccess
      */
     private $table;
- 
+
     public function __construct(ArrayAccess $table, $key, $prop)
     {
         $this->table = $table;
         $this->key = $key;
+
+        if (is_null($prop) || empty($prop)) {
+            throw new InvalidArgumentException('The `prop` argument is invalid');
+        }
+
         $this->prop = $prop;
-    }    
+    }
 
     public function read()
-    {       
+    {
+        if ( ! $this->table->offsetExists($this->key)) {
+            return NULL;
+        }
+
         $record = $this->table->offsetGet($this->key);
-        if(!isset($record[$this->prop])) {
-            return NULL;        
+        if ( ! isset($record[$this->prop])) {
+            return NULL;
         }
         return $record[$this->prop];
     }
 
     public function write($value)
     {
-        if(!isset($this->key)) {
+        if ( ! isset($this->key)) {
             throw new NethGui_Exception_Serializer('The TablePropSerializer `key` is not missing.');
         }
-        
-        $record = $this->table->offsetGet($this->key);
+
+        // update or append ?
+        if ($this->table->offsetExists($this->key)) {
+            $record = $this->table->offsetGet($this->key);
+        } else {
+            $record = array();
+        }
+
         $record[$this->prop] = $value;
         $this->table->offsetSet($this->key, $record);
     }
