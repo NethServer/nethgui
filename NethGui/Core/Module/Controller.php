8a6478363206c184344fd3e92f5b574c2965b51e
Davide Principi <davide.principi@nethesis.it>
2011-06-23 18:42:41 +0200

    Some code cleanup..

--- NethGui/Core/Module/Controller.php
+++ NethGui/Core/Module/Controller.php
@@ -95,7 +95,7 @@ class NethGui_Core_Module_Controller extends NethGui_Core_Module_Composite imple
     /**
      * Implements validate() method, forwarding the call to current action only.
      * @param NethGui_Core_ValidationReportInterface $report
-     * @return type 
+     * @return void 
      */
     public function validate(NethGui_Core_ValidationReportInterface $report)
     {
@@ -109,8 +109,7 @@ class NethGui_Core_Module_Controller extends NethGui_Core_Module_Composite imple
     /**
      * Implements process() method, forwarding the call to current 
      * action only
-     * @param NethGui_Core_NotificationCarrierInterface $carrier
-     * @return type 
+     * @return void 
      */
     public function process()
     {
