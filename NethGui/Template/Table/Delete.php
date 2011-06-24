98a4b5662b14fcf0cefe236c3fda3cec48815d93
Davide Principi <davide.principi@nethesis.it>
2011-06-24 17:21:42 +0200

    Wrapped delete action confirmation dialog text into a DIV tag

--- NethGui/Template/Table/Delete.php
+++ NethGui/Template/Table/Delete.php
@@ -1,13 +1,14 @@
-<?php
+<div class="dialog-text"><?php
 $message = T('Confirm deletion of "%s"?',
         array('%s' => empty($view[$view['__key']]) ? '%s' : $view[$view['__key']]), NULL, NULL, FALSE);
 
 // Render the dialog content
-echo $view
-        ->append($message) // Add the dialog text (see $message)
-        ->hidden($view['__key']) // Put the key value into an hidden control
+
+echo $view->append($message); // Add the dialog text (see $message)
+
+echo $view->hidden($view['__key']) // Put the key value into an hidden control
     ;
-?><ul class="actions"><li><?php 
+?></div><ul class="actions"><li><?php 
     echo $view->button('Submit', NethGui_Renderer_Xhtml::BUTTON_SUBMIT) // Add SUBMIT button 
         ?></li><li><?php
     echo $view->button('Cancel', NethGui_Renderer_Xhtml::BUTTON_CANCEL) // Add CANCEL button
