ed049b00ab5a34a35d03f907cf1c3cdecaf01b7c
Davide Principi <davide.principi@nethesis.it>
2011-06-24 17:35:41 +0200

    Refs rev:1d33c5ae9e. Re-wrapped delete dialog message text.

--- NethGui/Template/Table/Delete.php
+++ NethGui/Template/Table/Delete.php
@@ -5,10 +5,8 @@ $message = T('Confirm deletion of "%s"?',
 // Render the dialog content
 
 echo $view->append($message); // Add the dialog text (see $message)
-
-echo $view->hidden($view['__key']) // Put the key value into an hidden control
-    ;
-?></div><ul class="actions"><li><?php 
+?></div><?php echo $view->hidden($view['__key']); // Put the key value into an hidden control
+?><ul class="actions"><li><?php 
     echo $view->button('Submit', NethGui_Renderer_Xhtml::BUTTON_SUBMIT) // Add SUBMIT button 
         ?></li><li><?php
     echo $view->button('Cancel', NethGui_Renderer_Xhtml::BUTTON_CANCEL) // Add CANCEL button
