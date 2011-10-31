c7795f414d87e6a8fa544fd8f293bdad32456e2e
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2011-10-31 16:16:14 +0100

    Bigger nethserver header

--- Nethgui/Template/World.php
+++ Nethgui/Template/World.php
@@ -10,7 +10,7 @@
     </head>
     <body>
         <div id="allWrapper">
-            <div id="pageHeader"><?php echo $view->inset('NotificationArea') ?><a id="product" href="Dashboard" title='NethServer'></a><h1 id="ModuleTitle"><?php echo $pageTitle ?></h1><h2>NethServer</h2></div>
+            <div id="pageHeader"><?php echo $view->inset('NotificationArea') ?><a id="product" href="Dashboard" title='NethServer'></a><h1 id="ModuleTitle"><?php echo $pageTitle ?></h1><h2>'NethServer</h2></div>
 
             <div id="pageContent">
                 <div class="primaryContent" role="mainTask">
