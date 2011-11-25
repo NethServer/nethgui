bcda5f86e8099ab68a31c24ddf463ba3cf234c37
Davide Principi <davide.principi@nethesis.it>
2011-11-25 09:53:07 +0100

    Dropped CodeIgniter: no more dependencies on it.

--- Nethgui/Bootstrap.php
+++ Nethgui/Bootstrap.php
@@ -4,7 +4,7 @@
  *
  * (C) 2011 Nethesis S.r.l.
  *
- * Common config values to bootstrap CodeIgniter and Nethgui 
+ * Common config values to bootstrap Nethgui 
  *
  */
 date_default_timezone_set('UTC');
@@ -130,67 +130,3 @@ if (NETHGUI_APPLICATION == 'Test') {
 
     return $_nethgui_app();
 }
-
-/**
- * Start CodeIgniter framework
- */
-// Set the current directory correctly for CLI requests
-if (defined('STDIN')) {
-    chdir(dirname(NETHGUI_FILE));
-}
-
-// ENVIRONMENT is used by CodeIgniter and takes the same value:
-define('ENVIRONMENT', NETHGUI_ENVIRONMENT);
-
-$system_path = realpath('../CodeIgniter/system');
-$application_folder = realpath('../CodeIgniter/application');
-
-if (realpath($system_path) !== FALSE) {
-    $system_path = realpath($system_path) . '/';
-}
-
-// ensure there's a trailing slash
-$system_path = rtrim($system_path, '/') . '/';
-
-// Is the system path correct?
-if ( ! is_dir($system_path)) {
-    exit("Your system folder path does not appear to be set correctly. Please open the following file and correct this: " . pathinfo(NETHGUI_FILE, PATHINFO_BASENAME));
-}
-
-/*
- * -------------------------------------------------------------------
- *  Now that we know the path, set the main path constants
- * -------------------------------------------------------------------
- */
-// The name of THIS file
-if ( ! defined('SELF')) {
-    define('SELF', pathinfo(NETHGUI_FILE, PATHINFO_BASENAME));
-}
-
-// The PHP file extension
-define('EXT', '.php');
-
-// Path to the system folder
-define('BASEPATH', str_replace("\\", "/", $system_path));
-
-// Path to the front controller 
-define('FCPATH', str_replace(SELF, '', NETHGUI_FILE));
-
-// Name of the "system folder"
-define('SYSDIR', trim(strrchr(trim(BASEPATH, '/'), '/'), '/'));
-
-
-// The path to the "application" folder
-if (is_dir($application_folder)) {
-    define('APPPATH', $application_folder . '/');
-} else {
-    if ( ! is_dir(BASEPATH . $application_folder . '/')) {
-        exit("Your application folder path does not appear to be set correctly. Please open the following file and correct this: " . SELF);
-    }
-
-    define('APPPATH', BASEPATH . $application_folder . '/');
-}
-
-require_once BASEPATH . 'core/CodeIgniter' . EXT;
-
-
