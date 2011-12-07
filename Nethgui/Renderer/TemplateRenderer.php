20e843283b787690628c9820ac5802679f08e556
Davide Principi <davide.principi@nethesis.it>
2011-12-07 12:01:33 +0100

    Renderer package: moved WidgetFactoryInterface implementation out of Xhtml into TemplateRenderer.

--- Nethgui/Renderer/TemplateRenderer.php
+++ Nethgui/Renderer/TemplateRenderer.php
@@ -26,8 +26,14 @@ namespace Nethgui\Renderer;
  * @author Davide Principi <davide.principi@nethesis.it>
  * @since 1.0
  */
-class TemplateRenderer extends AbstractRenderer implements \Nethgui\Core\GlobalFunctionConsumerInterface
+abstract class TemplateRenderer extends AbstractRenderer implements \Nethgui\Core\GlobalFunctionConsumerInterface, WidgetFactoryInterface
 {
+
+    /**
+     *
+     * @var integer
+     */
+    protected $inheritFlags = 0;
 
     /**
      *
@@ -40,13 +46,16 @@ class TemplateRenderer extends AbstractRenderer implements \Nethgui\Core\GlobalF
      */
     protected $globalFunctionWrapper;
 
-    public function __construct(\Nethgui\Core\ViewInterface $view, $templateResolver)
+    abstract protected function createWidget($widgetType, $attributes);
+
+    public function __construct(\Nethgui\Core\ViewInterface $view, $templateResolver, $inheritFlags)
     {
         if ( ! is_callable($templateResolver)) {
             throw new \InvalidArgumentException(sprintf('%s: $templateResolver must be a valid callback function.', get_class($this)), 1322238847);
         }
         parent::__construct($view);
         $this->templateResolver = $templateResolver;
+        $this->inheritFlags = $inheritFlags & NETHGUI_INHERITABLE_FLAGS;
         $this->globalFunctionWrapper = new \Nethgui\Core\GlobalFunctionWrapper();
     }
 
@@ -96,6 +105,233 @@ class TemplateRenderer extends AbstractRenderer implements \Nethgui\Core\GlobalF
         }
 
         return $viewOutput;
+    }
+
+    public function getDefaultFlags()
+    {
+        return $this->inheritFlags;
+    }
+
+    public function setDefaultFlags($flags)
+    {
+        $this->inheritFlags = $flags;
+        return $this;
+    }
+
+    public function elementList($flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        $widget = $this->createWidget(__FUNCTION__, array('flags' => $flags));
+
+        if ($flags & self::BUTTONSET) {
+            $widget->setAttribute('class', 'Buttonset')
+                ->setAttribute('wrap', 'div/');
+        }
+
+        // Automatically add standard submit/reset/cancel buttons:
+        if ($flags & (self::BUTTON_SUBMIT | self::BUTTON_RESET | self::BUTTON_CANCEL | self::BUTTON_HELP)) {
+            if ( ! $widget->hasAttribute('class')) {
+                $widget->setAttribute('class', 'Buttonlist')
+                    ->setAttribute('wrap', 'div/');
+            }
+
+            if ($flags & self::BUTTON_SUBMIT) {
+                $widget->insert($this->button('Submit', self::BUTTON_SUBMIT));
+            }
+            if ($flags & self::BUTTON_RESET) {
+                $widget->insert($this->button('Reset', self::BUTTON_RESET));
+            }
+            if ($flags & self::BUTTON_CANCEL) {
+                $widget->insert($this->button('Cancel', self::BUTTON_CANCEL));
+            }
+            if ($flags & self::BUTTON_HELP) {
+                $widget->insert($this->button('Help', self::BUTTON_HELP));
+            }
+        }
+
+        return $widget;
+    }
+
+    public function buttonList($flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        $widget = $this->createWidget("elementList", array('flags' => $flags));
+
+        $widget->setAttribute('class', 'Buttonlist')->setAttribute('wrap', 'div/');
+
+        return $widget;
+    }
+
+    public function button($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function checkBox($name, $value, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'value' => $value, 'flags' => $flags));
+    }
+
+    public function dialog($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+
+        $className = 'dialog';
+
+        if ($flags & WidgetFactoryInterface::DIALOG_SUCCESS) {
+            $className .= ' success';
+        } elseif ($flags & WidgetFactoryInterface::DIALOG_WARNING) {
+            $className .= ' warning';
+        } elseif ($flags & WidgetFactoryInterface::DIALOG_ERROR) {
+            $className .= ' error';
+        }
+
+        if ($flags & WidgetFactoryInterface::DIALOG_MODAL) {
+            $className .= ' modal';
+        }
+
+        if ($flags & WidgetFactoryInterface::STATE_DISABLED) {
+            $className .= ' disabled';
+        }
+
+        /*
+         * Create a panel wrapped around the inset
+         */
+
+        $panel = $this->panel($flags)
+            ->setAttribute('class', $className)
+            ->setAttribute('name', $name);
+        $inset = $this->createWidget('inset', array('name' => $name, 'flags' => $flags));
+
+        $panel->insert($inset);
+
+        return $panel;
+    }
+
+    public function fieldsetSwitch($name, $value, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'value' => $value, 'flags' => $flags));
+    }
+
+    public function form($flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('flags' => $flags));
+    }
+
+    public function hidden($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function inset($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function panel($flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('flags' => $flags));
+    }
+
+    public function radioButton($name, $value, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'value' => $value, 'flags' => $flags));
+    }
+
+    public function selector($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags, 'icon-before' => 'ui-icon-triangle-1-s'));
+    }
+
+    public function tabs($flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('flags' => $flags));
+    }
+
+    public function textInput($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function textLabel($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function fieldset($name = NULL, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        $widget = $this->createWidget(__FUNCTION__, array('flags' => $flags, 'icon-before' => 'ui-icon-triangle-1-s'));
+        if ( ! is_null($name)) {
+            $widget->setAttribute('name', $name);
+        }
+        return $widget;
+    }
+
+    public function header($name = NULL, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        $widget = $this->createWidget('textLabel', array('flags' => $flags, 'class' => 'header ui-widget-header ui-corner-all ui-helper-clearfix', 'tag' => 'h2'));
+        if ( ! is_null($name)) {
+            $widget->setAttribute('name', $name);
+        }
+        return $widget;
+    }
+
+    public function literal($data, $flags = 0)
+    {
+        return $this->createWidget(__FUNCTION__, array('data' => $data, 'flags' => $flags));
+    }
+
+    public function columns()
+    {
+        return $this->createWidget(__FUNCTION__, array());
+    }
+
+    public function progressBar($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function textArea($name, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags));
+    }
+
+    public function console($name, $flags = 0)
+    {
+        $flags |= self::STATE_READONLY;
+        $flags |= self::LABEL_NONE;
+        return $this->textArea($name, $flags)->setAttribute('appendOnly', TRUE)->setAttribute('class', 'console');
+    }
+
+    public function dateInput($name, $flags = 0)
+    {
+        /*
+         * Set to "be" (Big Endian) date format. Supported also "le" and "me".
+         * see http://en.wikipedia.org/wiki/Calendar_date
+         */
+        return $this->textInput('date')->setAttribute('class', 'Date be');
+    }
+
+    public function objectPicker($name = NULL, $flags = 0)
+    {
+        $flags |= $this->inheritFlags;
+        return $this->createWidget(__FUNCTION__, array('name' => $name, 'flags' => $flags, 'icon-before' => 'ui-icon-triangle-1-s'));
     }
 
     public function setGlobalFunctionWrapper(\Nethgui\Core\GlobalFunctionWrapper $object)
