22b2252a5da83eedf5f0a6ed0353dc305d18613a
Davide Principi <davide.principi@nethesis.it>
2012-04-11 09:41:51 +0200

    navigation JS UI: implemented search-as-you-type behaviour. Closes #940 - Menu search "as you type"

--- Nethgui/Js/jquery.nethgui.navigation.js
+++ Nethgui/Js/jquery.nethgui.navigation.js
@@ -12,10 +12,34 @@
                 .button({icons: {primary: 'ui-icon-search'}, text: false})
                 .removeClass('ui-corner-all');
                 
-
-            this.element.find('.TextInput')
-                .removeClass('ui-corner-all');
-                
+            this._timer = false;
+            this._input = this.element.find('.TextInput')
+                .removeClass('ui-corner-all')
+                .bind("keyup paste", $.proxy(this._submitCheck, this));
+           
+            this._form.bind('submit', $.proxy(this._clearTimer, this));
+        },
+        _clearTimer: function () {            
+            if(this._timer !== false) {                
+                window.clearTimeout(this._timer);
+                this._timer = false;                
+            }      
+        },
+        _submitCheck: function(event) {
+            var self = this;
+                        
+            this._clearTimer();
+            
+            if(event.keyCode === $.ui.keyCode.ENTER) {
+                return true;
+            }
+                        
+            if(self._input.val().length > 1) {
+                this._timer = window.setTimeout(function () {                
+                    self._form.submit();
+                }, 600);
+            }
+                                               
         },
         _updateView: function(value) {
             // if the response is empty show all items:
