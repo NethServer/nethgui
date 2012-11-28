a9b7f5171e387960d06ec412db6cc0676d930a8d
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2012-11-28 17:38:50 +0100

    Translation: refactor  english and italian translation files #523

--- Nethgui/Language/en/Nethgui.php
+++ Nethgui/Language/en/Nethgui.php
@@ -1,103 +1,94 @@
-<?php
-namespace Nethgui\Language\en;
+<?php 
 
-/*
- * Copyright (C) 2011 Nethesis S.r.l.
- * 
- * This script is part of NethServer.
- * 
- * NethServer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * NethServer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with NethServer.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-$L['status_disabled_label'] = 'Disabled';
-$L['status_enabled_label'] = 'Enabled';
-
-$L['Confirm deletion of "%s"?'] = 'Confirm deletion of "%s"?';
-$L['Actions_label'] = 'Actions';
-$L['Submit_label'] = 'Submit';
-$L['Apply_label'] = 'Apply';
-$L['Cancel_label'] = 'Cancel';
-$L['Help_label'] = 'Help';
-$L['Hide_label'] = 'Hide';
-$L['Find_label'] = 'Find';
-$L['update_label'] = 'Update';
-$L['delete_label'] = 'Delete';
-$L['create_label'] = 'Create new';
-
-$L['Ignore_label'] = 'Ignore';
-$L['All changes have been saved'] = 'All changes have been saved';
-$L['Description_label'] = 'Description';
-
-$L['regexp "${0}"'] = 'regular expression \'${0}\'';
-
-$L['nethgui-test_success'] = 'Nethgui test event processed successfully (${0})';
-$L['nethgui-test_failure'] = 'Error in Nethgui test event processing (${0})';
-$L['Search'] = 'Search';
-
-$L['create_Title'] = 'Create';
-$L['read_Title'] = '';
-$L['update_Title'] = 'Update';
-$L['delete_Title'] = 'Delete';
-$L['Help_Title'] = 'Help';
-
-$L['username_label'] = 'User name';
-$L['password_label'] = 'Password';
-$L['language_label'] = 'Language';
-
-$L['Login_label'] = 'Login';
-$L['Logout_label'] = 'Logout';
-$L['Welcome on ${0}'] = 'Welcome on ${0}';
-
-$L['An object with the same key already exists'] = 'An object with the same key already exists';
-
-$L['Configure_label'] = 'Configure';
-
-$L['valid_email,too-long'] = 'email address too long';
-$L['valid_email,malformed-localpart'] = 'invalid email address';
-$L['valid_email,missing-localpart'] = 'invalid email address';
-$L['valid_email,missing-domainpart'] = 'invalid email address';
-$L['valid_email,malformed-domainpart'] = 'invalid email address';
-
-$L['Login_Title'] = 'Login';
-$L['Login_Description'] = 'Login';
-$L['Menu_Title'] = 'Menu';
-$L['Menu_Description'] = 'Menu';
-$L['Notification_Title'] = 'Notification';
-$L['Notification_Description'] = 'Notification';
-$L['Resource_Title'] = 'Resource';
-$L['Resource_Description'] = 'Resource';
-$L['Logout_Title'] = 'Logout';
-$L['Logout_Description'] = 'Logout';
-
-$L['Logout_Tags'] = '';
-$L['Menu_Tags'] = '';
-$L['Resource_Tags'] = '';
-$L['Notification_Tags'] = '';
-$L['Login_Tags'] = '';
-
-$L['search_label']='Search';
-$L['Delete'] = 'Delete';
-$L['Update'] = 'Update';
-$L['Done'] = 'Done';
-$L['Empty table'] = 'Empty table';
-$L['Delete `${0}`'] = 'Delete `${0}`';
-$L['Confirm deletion of `${0}`?'] = 'Confirm deletion of `${0}`';
-
-
-$L['Add'] = 'Add';
-$L['Search...'] = 'Search...';
-$L['Yes_label'] = 'Yes';
-$L['No_label'] = 'No';
-
-
+$L['Actions_label'] = 'Actions'; 
+$L['Add'] = 'Add'; 
+$L['All changes have been saved'] = 'All changes have been saved'; 
+$L['An object with the same key already exists'] = 'An object with the same key already exists'; 
+$L['Apply_label'] = 'Apply'; 
+$L['Cancel_label'] = 'Cancel'; 
+$L['Configure_label'] = 'Configure'; 
+$L['Confirm deletion of `${0}`?'] = 'Confirm deletion of `${0}`'; 
+$L['Delete'] = 'Delete'; 
+$L['Delete `${0}`'] = 'Delete `${0}`'; 
+$L['Describe <tt>${0}</tt> here..'] = 'Describe <tt>${0}</tt> here..'; 
+$L['Description_label'] = 'Description'; 
+$L['Documents'] = 'Documents'; 
+$L['Done'] = 'Done'; 
+$L['Empty table'] = 'Empty table'; 
+$L['Find_label'] = 'Find'; 
+$L['Help_Title'] = 'Help'; 
+$L['Help_label'] = 'Help'; 
+$L['Hide_label'] = 'Hide'; 
+$L['Ignore_label'] = 'Ignore'; 
+$L['Incorrect value'] = 'Incorrect value'; 
+$L['Incorrect values'] = 'Incorrect values'; 
+$L['Invalid credentials'] = 'Invalid credentials'; 
+$L['Login_Description'] = 'Login'; 
+$L['Login_Tags'] = ''; 
+$L['Login_Title'] = 'Login'; 
+$L['Login_label'] = 'Login'; 
+$L['Logout_Description'] = 'Logout'; 
+$L['Logout_Tags'] = ''; 
+$L['Logout_Title'] = 'Logout'; 
+$L['Logout_label'] = 'Logout'; 
+$L['Menu_Description'] = 'Menu'; 
+$L['Menu_Tags'] = ''; 
+$L['Menu_Title'] = 'Menu'; 
+$L['No_label'] = 'No'; 
+$L['Notification_Description'] = 'Notification'; 
+$L['Notification_Tags'] = ''; 
+$L['Notification_Title'] = 'Notification'; 
+$L['Ok_label'] = 'Ok'; 
+$L['Please wait...'] = 'Please wait...'; 
+$L['Reset_label'] = 'Reset'; 
+$L['Resource_Description'] = 'Resource'; 
+$L['Resource_Tags'] = ''; 
+$L['Resource_Title'] = 'Resource'; 
+$L['Search'] = 'Search'; 
+$L['Search...'] = 'Search...'; 
+$L['Submit_label'] = 'Submit'; 
+$L['Templates'] = 'Templates'; 
+$L['Update'] = 'Update'; 
+$L['Welcome on ${0}'] = 'Welcome on ${0}'; 
+$L['Yes_label'] = 'Yes'; 
+$L['create_Title'] = 'Create'; 
+$L['create_label'] = 'Create new'; 
+$L['delete_Title'] = 'Delete'; 
+$L['delete_label'] = 'Delete'; 
+$L['language_label'] = 'Language'; 
+$L['nethgui-test_failure'] = 'Error in Nethgui test event processing (${0})'; 
+$L['nethgui-test_success'] = 'Nethgui test event processed successfully (${0})'; 
+$L['password_label'] = 'Password'; 
+$L['read_Title'] = ''; 
+$L['search_label'] = 'Search'; 
+$L['status_disabled_label'] = 'Disabled'; 
+$L['status_enabled_label'] = 'Enabled'; 
+$L['update_Title'] = 'Update'; 
+$L['update_label'] = 'Update'; 
+$L['username_label'] = 'User name'; 
+$L['valid_OR'] = 'or'; 
+$L['valid_date'] = 'date format `${0}`'; 
+$L['valid_email,malformed-domainpart'] = 'invalid email address'; 
+$L['valid_email,malformed-localpart'] = 'invalid email address'; 
+$L['valid_email,missing-domainpart'] = 'invalid email address'; 
+$L['valid_email,missing-localpart'] = 'invalid email address'; 
+$L['valid_email,too-long'] = 'email address too long'; 
+$L['valid_equalTo'] = 'equal to ${0}'; 
+$L['valid_greatThan'] = 'greater than ${0}'; 
+$L['valid_hostname'] = 'host name'; 
+$L['valid_hostname_fqdn'] = 'host name with domain'; 
+$L['valid_hostname_simple'] = 'host name without domain'; 
+$L['valid_integer'] = 'integer number'; 
+$L['valid_ipV4Address'] = 'IPv4 valid address'; 
+$L['valid_ipV4Netmask'] = 'IPv4 valid mask'; 
+$L['valid_isEmpty'] = 'empty'; 
+$L['valid_lessThan'] = 'lesser than ${0}'; 
+$L['valid_macAddress'] = 'valid MAC address'; 
+$L['valid_maxLength'] = 'maximum length ${0}'; 
+$L['valid_memberOf'] = 'value between ${0}'; 
+$L['valid_minLength'] = 'minimum length ${0}'; 
+$L['valid_notEmpty'] = 'not empty'; 
+$L['valid_positive'] = 'positive number'; 
+$L['valid_regexp'] = 'regular expression ${0}'; 
+$L['valid_time'] = 'time format `HH:MM[:SS]`'; 
+$L['valid_username'] = 'first letter, than only lower letters, numbers and symbols like "-" and "_"'; 
--- Nethgui/Language/it/Nethgui.php
+++ Nethgui/Language/it/Nethgui.php
@@ -1,130 +1,96 @@
-<?php
-namespace Nethgui\Language\it;
+<?php 
 
-/*
- * Copyright (C) 2011 Nethesis S.r.l.
- * 
- * This script is part of NethServer.
- * 
- * NethServer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- * 
- * NethServer is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with NethServer.  If not, see <http://www.gnu.org/licenses/>.
- */
-
-$L['status_disabled_label'] = 'Disabilitato';
-$L['status_enabled_label'] = 'Abilitato';
-
-$L['Confirm deletion of `${0}`?'] = 'Confermi l\'eliminazione di `${0}`?';
-$L['Delete `${0}`'] = 'Elimina "${0}"';
-$L['Actions_label'] = 'Azioni';
-$L['Submit_label'] = 'Salva';
-$L['Apply_label'] = 'Applica';
-$L['Cancel_label'] = 'Annulla';
-$L['Help_label'] = 'Aiuto';
-$L['Hide_label'] = 'Nascondi';
-$L['Find_label'] = 'Trova';
-$L['update_label'] = 'Modifica';
-$L['delete_label'] = 'Elimina';
-$L['create_label'] = 'Crea nuovo';
-$L['Yes_label'] = 'Si'; 
+$L['Actions_label'] = 'Azioni'; 
+$L['Add'] = 'Aggiungi'; 
+$L['All changes have been saved'] = 'Tutte le modifiche sono state salvate'; 
+$L['An object with the same key already exists'] = 'Un oggetto con lo stesso identificatore esiste già'; 
+$L['Apply_label'] = 'Applica'; 
+$L['Cancel_label'] = 'Annulla'; 
+$L['Configure_label'] = 'Configura'; 
+$L['Confirm deletion of "%s"?'] = 'Confermi l\'eliminazione di "%s"?'; 
+$L['Confirm deletion of `${0}`?'] = 'Confermi l\'eliminazione di `${0}`?'; 
+$L['Delete'] = 'Elimina'; 
+$L['Delete `${0}`'] = 'Elimina "${0}"'; 
+$L['Describe <tt>${0}</tt> here..'] = 'Descrivi <tt>${0}</tt> qui..'; 
+$L['Description_label'] = 'Descrizione'; 
+$L['Documents'] = 'Documenti'; 
+$L['Done'] = 'Fatto'; 
+$L['Empty table'] = 'Tabella vuota'; 
+$L['Find_label'] = 'Trova'; 
+$L['Help_Title'] = 'Guida'; 
+$L['Help_label'] = 'Aiuto'; 
+$L['Hide_label'] = 'Nascondi'; 
+$L['Ignore_label'] = 'Ignora'; 
+$L['Incorrect value'] = 'Valore non corretto'; 
+$L['Incorrect values'] = 'Valori non corretti'; 
+$L['Invalid credentials'] = 'Credenziali non valide'; 
+$L['Login_Description'] = 'Login'; 
+$L['Login_Tags'] = ''; 
+$L['Login_Title'] = 'Login'; 
+$L['Login_label'] = 'Accedi'; 
+$L['Logout_Description'] = 'Logout'; 
+$L['Logout_Tags'] = ''; 
+$L['Logout_Title'] = 'Logout'; 
+$L['Logout_label'] = 'Esci'; 
+$L['Menu_Description'] = 'Menu'; 
+$L['Menu_Tags'] = ''; 
+$L['Menu_Title'] = 'Menu'; 
 $L['No_label'] = 'No'; 
+$L['Notification_Description'] = 'Notification'; 
+$L['Notification_Tags'] = ''; 
+$L['Notification_Title'] = 'Notification'; 
 $L['Ok_label'] = 'Ok'; 
-$L['Ignore_label'] = 'Ignora';
-$L['All changes have been saved'] = 'Tutte le modifiche sono state salvate';
-$L['Reset_label'] = 'Ripristina';
-$L['Description_label'] = 'Descrizione';
-
-
-$L['valid_regexp'] = 'espressione regolare ${0}';
-$L['valid_positive'] = 'numero positivo';
-$L['valid_integer'] = 'numero intero';
-$L['valid_ipV4Address'] = 'indirizzo IPv4 valido';
-$L['valid_ipV4Netmask'] = 'maschera IPv4 valida';
-$L['valid_username'] = 'lettera iniziale, poi solo lettere minuscole, numeri, simboli "-" e "_"';
-$L['valid_notEmpty'] = 'non vuoto';
-$L['valid_OR'] = 'oppure';
-$L['valid_hostname'] = 'nome host';
-$L['valid_hostname_fqdn'] = 'nome host con dominio';
-$L['valid_hostname_simple'] = 'nome host senza dominio';
-$L['valid_time'] = 'formato ora `HH:MM[:SS]`';
-$L['valid_date'] = 'formato data `${0}`';
-$L['valid_minLength'] = 'lunghezza minima ${0} caratteri';
-$L['valid_maxLength'] = 'lunghezza massima ${0} caratteri';
-$L['valid_greatThan'] = 'maggiore di ${0}';
-$L['valid_lessThan'] = 'minore di ${0}';
-$L['valid_equalTo'] = 'uguale a ${0}';
-$L['valid_memberOf'] = 'un valore tra ${0}';
-$L['valid_isEmpty'] = 'vuoto';
-$L['valid_macAddress'] = 'indirizzo MAC valido';
-$L['valid_email,too-long'] = 'indirizzo email troppo lungo';
-$L['valid_email,malformed-localpart'] = 'indirizzo email non valido';
-$L['valid_email,missing-localpart'] = 'indirizzo email non valido';
-$L['valid_email,missing-domainpart'] = 'indirizzo email non valido';
-$L['valid_email,malformed-domainpart'] = 'indirizzo email non valido';
-
-$L['Incorrect value'] = 'Valore non corretto';
-$L['Incorrect values'] = 'Valori non corretti';
-
-$L['nethgui-test_success'] = 'Evento di test Nethgui eseguito con successo (${0})';
-$L['nethgui-test_failure'] = 'Errore durante l\'evento di test Nethgui (${0})';
-
-$L['Please wait...'] = 'Attendere prego...';
-$L['Search'] = 'Cerca';
-$L['Search...'] = 'Cerca...';
-$L['Add'] = 'Aggiungi';
-
-$L['create_Title'] = 'Crea';
-$L['read_Title'] = '';
-$L['update_Title'] = 'Modifica';
-$L['delete_Title'] = 'Elimina';
-$L['Help_Title'] = 'Guida';
-
-$L['Describe <tt>${0}</tt> here..'] = 'Descrivi <tt>${0}</tt> qui..';
-$L['Documents'] = 'Documenti';
-$L['Templates'] = 'Template';
-
-$L['username_label'] = 'Nome utente';
-$L['password_label'] = 'Password';
-$L['language_label'] = 'Lingua';
-
-$L['Login_label'] = 'Accedi';
-$L['Logout_label'] = 'Esci';
-$L['Welcome on ${0}'] = 'Benvenuto su ${0}';
-$L['Invalid credentials'] = 'Credenziali non valide';
-$L['Empty table'] = 'Tabella vuota';
-
-$L['An object with the same key already exists'] = 'Un oggetto con lo stesso identificatore esiste già';
-
-$L['Configure_label'] = 'Configura';
-
-$L['Login_Title'] = 'Login';
-$L['Login_Description'] = 'Login';
-$L['Menu_Title'] = 'Menu';
-$L['Menu_Description'] = 'Menu';
-$L['Notification_Title'] = 'Notification';
-$L['Notification_Description'] = 'Notification';
-$L['Resource_Title'] = 'Resource';
-$L['Resource_Description'] = 'Resource';
-$L['Logout_Title'] = 'Logout';
-$L['Logout_Description'] = 'Logout';
-
-$L['Logout_Tags'] = '';
-$L['Menu_Tags'] = '';
-$L['Resource_Tags'] = '';
-$L['Notification_Tags'] = '';
-$L['Login_Tags'] = '';
-
-$L['search_label']='Cerca';
-$L['Delete'] = 'Elimina';
-$L['Update'] = 'Aggiorna';
-$L['Done'] = 'Fatto';
-
+$L['Please wait...'] = 'Attendere prego...'; 
+$L['Reset_label'] = 'Ripristina'; 
+$L['Resource_Description'] = 'Resource'; 
+$L['Resource_Tags'] = ''; 
+$L['Resource_Title'] = 'Resource'; 
+$L['Search'] = 'Cerca'; 
+$L['Search...'] = 'Cerca...'; 
+$L['Submit_label'] = 'Salva'; 
+$L['Templates'] = 'Template'; 
+$L['Update'] = 'Aggiorna'; 
+$L['Welcome on ${0}'] = 'Benvenuto su ${0}'; 
+$L['Yes_label'] = 'Si'; 
+$L['create_Title'] = 'Crea'; 
+$L['create_label'] = 'Crea nuovo'; 
+$L['delete_Title'] = 'Elimina'; 
+$L['delete_label'] = 'Elimina'; 
+$L['language_label'] = 'Lingua'; 
+$L['nethgui-test_failure'] = 'Errore durante l\'evento di test Nethgui (${0})'; 
+$L['nethgui-test_success'] = 'Evento di test Nethgui eseguito con successo (${0})'; 
+$L['password_label'] = 'Password'; 
+$L['read_Title'] = ''; 
+$L['regexp "${0}"'] = 'espressione regolare \'${0}\''; 
+$L['search_label'] = 'Cerca'; 
+$L['status_disabled_label'] = 'Disabilitato'; 
+$L['status_enabled_label'] = 'Abilitato'; 
+$L['update_Title'] = 'Modifica'; 
+$L['update_label'] = 'Modifica'; 
+$L['username_label'] = 'Nome utente'; 
+$L['valid_OR'] = 'oppure'; 
+$L['valid_date'] = 'formato data `${0}`'; 
+$L['valid_email,malformed-domainpart'] = 'indirizzo email non valido'; 
+$L['valid_email,malformed-localpart'] = 'indirizzo email non valido'; 
+$L['valid_email,missing-domainpart'] = 'indirizzo email non valido'; 
+$L['valid_email,missing-localpart'] = 'indirizzo email non valido'; 
+$L['valid_email,too-long'] = 'indirizzo email troppo lungo'; 
+$L['valid_equalTo'] = 'uguale a ${0}'; 
+$L['valid_greatThan'] = 'maggiore di ${0}'; 
+$L['valid_hostname'] = 'nome host'; 
+$L['valid_hostname_fqdn'] = 'nome host con dominio'; 
+$L['valid_hostname_simple'] = 'nome host senza dominio'; 
+$L['valid_integer'] = 'numero intero'; 
+$L['valid_ipV4Address'] = 'indirizzo IPv4 valido'; 
+$L['valid_ipV4Netmask'] = 'maschera IPv4 valida'; 
+$L['valid_isEmpty'] = 'vuoto'; 
+$L['valid_lessThan'] = 'minore di ${0}'; 
+$L['valid_macAddress'] = 'indirizzo MAC valido'; 
+$L['valid_maxLength'] = 'lunghezza massima ${0} caratteri'; 
+$L['valid_memberOf'] = 'un valore tra ${0}'; 
+$L['valid_minLength'] = 'lunghezza minima ${0} caratteri'; 
+$L['valid_notEmpty'] = 'non vuoto'; 
+$L['valid_positive'] = 'numero positivo'; 
+$L['valid_regexp'] = 'espressione regolare ${0}'; 
+$L['valid_time'] = 'formato ora `HH:MM[:SS]`'; 
+$L['valid_username'] = 'lettera iniziale, poi solo lettere minuscole, numeri, simboli "-" e "_"'; 
