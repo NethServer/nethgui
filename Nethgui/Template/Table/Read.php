8856186e897145324dd3422a6c3cf0f29023fadc
Davide Principi <davide.principi@nethesis.it>
2012-11-30 16:41:02 +0100

    Read Template: code refactor. Refs #1574

--- Nethgui/Template/Table/Read.php
+++ Nethgui/Template/Table/Read.php
@@ -1,47 +1,45 @@
 <?php
-    // skip tables on HELP templates
-    if($view instanceof \Nethgui\Module\Help\Renderer) {
-        return;
-    }
+// skip tables on HELP templates
+if ($view instanceof \Nethgui\Module\Help\Renderer) {
+    return;
+}
 
-    $view
-        ->includeFile('Nethgui/Js/jquery.nethgui.datatable.js')
-        ->rejectFlag($view::INSET_FORM)
-    ;
+/* @var $view Nethgui\Renderer\Xhtml */
 
-//    if(strlen($view['tableTitle']) > 0) {
-//        echo $view->header()->setAttribute('template', $view['tableTitle']);
-//    }
-    
-    echo $view->literal($view['TableActions'])
+$view
+    ->includeFile('Nethgui/Js/jquery.nethgui.datatable.js')
+    ->rejectFlag($view::INSET_FORM)
+;
 
-?><div class="DataTable <?php echo $view['tableClass']?>" ><table>
-    <thead><tr>
-        <?php
-            foreach ($view['columns'] as $columnInfo) {
-                echo isset($columnInfo['formatter']) ? '<th class="' . $columnInfo['formatter'] . '">' : '<th>';
-                echo htmlspecialchars($view->translate($columnInfo['name'] . '_label'));
-                echo '</th>';
-            }
-        ?>
-    </tr></thead>
-    <tbody><?php if(count($view['rows']) > 0) : 
-        foreach ($view['rows'] as $rowId => $row): ?>
-    <tr class='<?php echo $row[0]['rowCssClass'];?>' >
-        <?php foreach ($row as $colId => $value): 
-              if($colId == 0) continue;  ?>
-             <td><?php echo $view->literal($value) ?></td>
-        <?php endforeach; ?>
-    </tr>
-    <?php endforeach; else: echo '<tr class="empty">'; 
-    
-    for($i = 0; $i < count($view['columns']); $i ++) {
-        if($i === 0) {
-            echo '<td><p>' . htmlspecialchars($view->translate('Empty table')) . '</p></td>';
-        } else {
-            echo '<td></td>';
-        }     
-    }
-    
-    echo '</tr>'; endif ?></tbody>
-</table></div>
+echo $view->literal($view['TableActions']);
+?><div class="DataTable <?php echo $view['tableClass'] ?>" ><table><thead><tr><?php
+foreach ($view['columns'] as $columnInfo) {
+    echo isset($columnInfo['formatter']) ? '<th class="' . $columnInfo['formatter'] . '">' : '<th>';
+    echo htmlspecialchars($view->translate($columnInfo['name'] . '_label'));
+    echo '</th>';
+}
+?></tr></thead><tbody><?php
+                if (count($view['rows']) > 0) {
+                    foreach ($view['rows'] as $rowId => $row) {
+                        echo '<tr class="' . $row[0]['rowCssClass'] . '">';
+                        foreach ($row as $colId => $value) {
+                            if ($colId == 0) {
+                                continue;
+                            } else {
+                                echo "<td>" . $view->literal($value) . "</td>";
+                            }
+                        }
+                        echo '</tr>';
+                    }
+                } else {
+                    echo '<tr class="empty">';
+                    for ($i = 0; $i < count($view['columns']); $i ++ ) {
+                        if ($i === 0) {
+                            echo '<td><p>' . htmlspecialchars($T('Empty table')) . '</p></td>';
+                        } else {
+                            echo '<td></td>';
+                        }
+                    }
+                    echo '</tr>';
+                }
+?></tbody></table></div>
