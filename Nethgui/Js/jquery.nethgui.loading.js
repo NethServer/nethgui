76d7a7e8d5af9cc4771dfbe2ae8af742205e698f
Davide Principi <davide.principi@nethesis.it>
2013-07-12 17:50:06 +0200

    Js UI (jquery.nethgui.loading.js): Removed ajax requests counter because ajaxStop event is signalled when no more requests are active. Refs# 1748

--- Nethgui/Js/jquery.nethgui.loading.js
+++ Nethgui/Js/jquery.nethgui.loading.js
@@ -15,8 +15,6 @@
 
     var dialog = $('<div id="NethguiOverlayLoadingMessage" style="display: none"/>');
 
-    var activeRequests = 0;
-
     $('body').append(dialog);
     dialog.dialog({
         autoOpen: false,
@@ -30,11 +28,8 @@
         if(! dialog.dialog('isOpen')) {
             dialog.dialog('open');
         }
-        activeRequests += 1;
     }).bind("ajaxStop.nethgui", function() {
-        activeRequests -= 1;
-        if(dialog.dialog('isOpen') && activeRequests < 1) {
-            activeRequests = 0;
+        if(dialog.dialog('isOpen')) {
             dialog.dialog('close');
         }
     });
