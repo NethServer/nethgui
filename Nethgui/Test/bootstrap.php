90fb403dc528eaf3b55338925a049a39b0989b56
Davide Principi <davide.principi@nethesis.it>
2013-10-30 15:05:05 +0100

    Use Composer PSR-0 class loader for unit tests. Refs #2293

--- Nethgui/Test/bootstrap.php
+++ Nethgui/Test/bootstrap.php
@@ -15,7 +15,7 @@ error_reporting(E_ALL | E_STRICT);
 // If xdebug is loaded, disable xdebug backtraces:
 extension_loaded('xdebug') && xdebug_disable();
 
-require_once(realpath(dirname(__FILE__) . '/../') . '/Framework.php');
+include_once('vendor/autoload.php');
 
 $FW = new \Nethgui\Framework();
 $FW       
