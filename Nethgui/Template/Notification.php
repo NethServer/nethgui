51487463c89c8a6732407aae6804a86dcc422dd4
Alessia Rossini <alessia.rossini@nethesis.it>
2016-05-31 10:17:09 +0200

    Notification: move CSS to base.css

--- Nethgui/Template/Notification.php
+++ Nethgui/Template/Notification.php
@@ -18,26 +18,6 @@ $t['validationError'] = array('<i class="fa fa-li fa-exclamation-triangle"></i>
 </dl>
 ', 'validationError bg-red pre-fa');
 
-$view->includeCss("
-#Notification { margin-bottom: 0.5em; font-size: 1.2em }
-#Notification .fa-ul { margin: 0 }
-#Notification > ul > li { padding: 1em; margin-bottom: 0.5em; }
-#Notification ul.fa-ul > li { padding-left: 2.5em }
-#Notification ul.fa-ul > li.nolpad { padding-left: .5em }
-#Notification .fa-li { font-size: 1.2em; left: 0; top: .8em }
-
-.notification.bg-red {color: #fff; background-color: #cd0a0a; border-color: #cd0a0a}
-.notification.bg-red a {color: #fff}
-
-.notification.bg-green {color: #fff; background-color: #00a21a; border-color: #00a21a }
-.notification.bg-green a {color: #fff}
-
-.notification.bg-yellow {color: #000; background-color: #F4D622; border-color: #F4D622 }
-.notification.bg-yellow a {color: #000}
-
-.notification.validationError dd {margin-bottom: .25em}
-");
-
 $escc = json_encode($colorMap);
 $jsCode = "\n    $.nethgui.Notification.colors = $escc;";
 foreach($t as $templateName => $templateValue) {
